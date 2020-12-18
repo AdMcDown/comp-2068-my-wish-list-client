7df6012e4cc2874d5176eba81679b9634e1cc931
Adam McNown <adammcnown@gmail.com>
2020-12-18 15:50:45 -0500

    delete not working without things i was told to delete from deploy...

--- src/components/Wishs/Destroy/index.jsx
+++ src/components/Wishs/Destroy/index.jsx
@@ -1,16 +1,18 @@
 import Axios from 'axios';
 import React, { useEffect, useContext } from 'react';
 import { Redirect, useParams } from 'react-router-dom';
+import { UserContext } from '../../Authentication/UserProvider';
 import { GlobalStoreContext } from '../../shared/Globals';
 import { NotificationContext } from '../../shared/Notifications';
 
 const Destroy = () => {
     const { id } = useParams();
+    const { user } = useContext(UserContext);
     const { globalStore } = useContext(GlobalStoreContext);
     const { setNotification } = useContext(NotificationContext);
 
     useEffect(() => {
-        Axios.post(`${globalStore.REACT_APP_ENDPOINT}/wishs/destroy`, { _id: id })
+        Axios.post(`${globalStore.REACT_APP_ENDPOINT}/wishs/destroy`, { _id: id, secret_token: (user && user.token) })
             .then(() => {
                 setNotification(`Wish was destroyed successfully.`);
             })
